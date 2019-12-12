01eb7a40a3e8e3741921e0d6af6fb4c10b43bbb1
anutka0306 <anutka0306@mail.ru>
2019-12-12 10:43:04 +0300

    catalog page

--- views/goods.php
+++ views/goods.php
@@ -3,10 +3,18 @@
 /**
  * @var App\modules\Good [] $goods
  */
-
+?>
+<div class="catalog_wrap">
+<?php
 foreach ($goods as $good) : ?>
-    <h1><?=$good->name?></h1>
+<div class="catalog_item">
+    <h3><a href="?c=good&a=one&id=<?=$good->id?>"><?=$good->name?></a></h3>
+    <a href="?c=good&a=one&id=<?=$good->id?>">
+        <img src="../<?=$good->image?>" alt="<?=$good->description?>" class="catalog_item-img">
+    </a>
+</div>
 <?php
 endforeach;
 ?>
+</div>
 
