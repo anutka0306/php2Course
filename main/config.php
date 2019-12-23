5a51a630c0e1ac1630ed25586b20687fb1362e30
anutka0306 <anutka0306@mail.ru>
2019-12-23 22:27:08 +0300

    config file start

---
+++ main/config.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    'name' => 'My Shop',
+    'defaultController' => 'user',
+
+    'components' => [
+        'db' => [
+          'class' => \App\services\DB::class,
+           'config' => [
+               'driver'=>'mysql',
+               'host'=>'localhost',
+               'db'=>'catalogl6',
+               'charset'=>'UTF8',
+               'username'=>'root',
+               'password'=>'root'
+           ],
+        ],
+        'render' => [
+          'class' => \App\services\renders\TwigRender::class,
+        ],
+        'request' => [
+          'class' => \App\services\Request::class,
+        ],
+        'userRepository' => [
+          'class' => \App\repositories\UserRepository::class,
+        ],
+    ],
+];
